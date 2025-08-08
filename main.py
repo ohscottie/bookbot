fd0d5c72fb875627bc992b6cfa78005cf30f0def
scottie <183048101+ohscottie@users.noreply.github.com>
2025-08-07 20:52:43 -0700

    feat: read file path from command line

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
+import sys
 from stats import get_num_words, get_num_char
 
-def get_book_text(filepath):
-    with open(filepath) as f:
+def get_book_text(file_path):
+    with open(file_path) as f:
         file_contents = f.read()
         return file_contents
 
 def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    file_path = sys.argv[1]
+
     print("============ BOOKBOT ============")
 
-    filepath = "books/frankenstein.txt"
-    print(f"Analyzing book found at {filepath}...")
-    book_text = get_book_text(filepath)
+    print(f"Analyzing book found at {file_path}...")
+    book_text = get_book_text(file_path)
     
     num_words = get_num_words(book_text)
     print("----------- Word Count ----------")
